1bd9da9c4af99f18e620d3d8a6a2579fed4bf0af
JuanJoven01 <juanjov95@hotmail.com>
2025-02-15 17:38:32 -0500

    feat(vite config -  index.css): proxy and tailwind
    Add the config ti vite, setting proxy, envs and tailwind css

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+
+    return defineConfig({
+      plugins: [
+        react(),
+        tailwindcss(),],
+
+        server: {
+            proxy: {
+              '/api': {
+              target: env.VITE_API_URL,
+              changeOrigin: true,
+              // rewrite: (path) => path.replace(/^\/api/, '/api'),
+      },
+            }
+        },
+    });
+}
+
